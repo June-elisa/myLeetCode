facbe5cde00d14ea7062be9c2c0cb68629401c80
Freya <ylian06@qq.com>
2022-04-27 16:06:49 +0800

    两个字典的交集新增map解法

--- 两个数组的交集/index.js
+++ 两个数组的交集/index.js
@@ -12,8 +12,19 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersection = function (nums1, nums2) {
+// 方法一：用集合set求解
+/* var intersection = function (nums1, nums2) {
     return [...new Set(nums1.filter(item => nums2.includes(item)))]
+};
+ */
+
+// 方法二：用字典 map求解
+var intersection = function (nums1, nums2) {
+    let map = new Map();
+    nums1.forEach(item => map.set(item, true))
+    return nums2.filter(item => {
+        return map.get(item) && map.delete(item)
+    })
 };
 
 let res = intersection([1, 2, 3, 1], [2, 2])
